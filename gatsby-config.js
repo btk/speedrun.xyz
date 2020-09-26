5eb419192c035d2953abd3b904e054050551aed7
Burak Tokak <iletisim@buraktokak.com>
2020-09-26 11:31:00 +0300

    update assets, add analyrtics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "UA-98344633-1",
+        trackingId: "UA-179101278-1",
       },
     },
     {
